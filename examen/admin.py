cca53e81062e309a1d74c43fb2fd0d20d19f46e4
Fede Traversi <federicotraversir99@hotmail.com>
2025-06-06 15:02:29 -0300

    Se añadieron los respectivos formularios de administrador para las Materias y los Horarios.

--- examen/admin.py
+++ examen/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Materia, Horario
 
-# Register your models here.
+@admin.register(Materia)
+class MateriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'creditos', 'semestre', 'docente')
+    ordering = ['semestre']
+    search_fields = ['nombre']
+    list_filter = ['nombre']
+
+@admin.register(Horario)
+class HorarioAdmin(admin.ModelAdmin):
+    list_display = ('materia', 'dia_semana', 'hora_inicio', 'hora_fin', 'aula')
+    ordering = ['dia_semana']
